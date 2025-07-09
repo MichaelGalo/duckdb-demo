aaea12fca1d7336c11e64f89bda81632ab63e4b1
Michael Galo <michael@michaelthinks.co>
2025-07-09 14:17:16 -0500

    added new logic

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-# Template Repo
-
-This is a starter template for Python projects.
-
-## Structure
-
-- `src/`: Source code
-- `tests/`: Unit tests
-- `data/`: Data files
-# duckdb-demo
+# DuckDB Demo
--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 import duckdb
 
-conn = duckdb.connect("local_db.duckdb")
-conn.execute("CREATE TABLE transactions AS SELECT * FROM 'data/transactions.parquet'")
-
 
 def main():
-    print("Hello World!")
+    # line 4 will create the db if it doesn't already exist
+    conn = duckdb.connect("local_db.duckdb")
+    conn.execute(
+        "CREATE TABLE transactions AS SELECT * FROM 'data/transactions.parquet'"
+    )
+    transactions_df = conn.execute("SELECT * FROM transactions").df()
+    transactions_df["amount"] = transactions_df["amount"] * 1.1
+    transactions_df.to_parquet("data/transactions_transformed.parquet", index=False)
+
+    memory_conn = duckdb.connect(":memory:")
+    memory_conn.execute(
+        "CREATE TABLE transactions AS SELECT * FROM 'data/transactions.parquet'"
+    )
+    mem_transactions_df = memory_conn.execute("SELECT * FROM transactions").df()
+    mem_transactions_df["amount"] = mem_transactions_df["amount"] * 1.1
+    mem_transactions_df.to_parquet(
+        "data/transactions_transformed_memory.parquet", index=False
+    )
 
 
 if __name__ == "__main__":
