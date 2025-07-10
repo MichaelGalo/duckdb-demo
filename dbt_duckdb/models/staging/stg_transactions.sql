b004325e054b9250fab16c2233d27b4d36ec45a8
Michael Galo <michael@michaelthinks.co>
2025-07-10 10:41:51 -0500

    added edits to dbt_duckdb models and yaml

---
+++ dbt_duckdb/models/staging/stg_transactions.sql
@@ -0,0 +1,17 @@
+select
+  -- Add any column cleaning/renaming here
+  transaction_id,
+  customer_id,
+  cast(amount as decimal(10,2)) as amount,
+  cast(transaction_date as date) as transaction_date,
+
+  -- Add any calculated fields
+  case
+    when amount > 0 then 'credit'
+    else 'debit'
+  end as transaction_type,
+
+  -- Metadata
+  current_timestamp as _loaded_at
+
+from main.transactions
