cd15bd3482c1e10a996305b5e222e83e855a389d
Michael Galo <michael@michaelthinks.co>
2025-07-10 09:48:43 -0500

    warm up finished

--- src/main.py
+++ src/main.py
@@ -1,26 +1,5 @@
-import duckdb
-
-
 def main():
-    # line 4 will create the db if it doesn't already exist
-    conn = duckdb.connect("./data/local_db.duckdb")
-    conn.execute(
-        "CREATE TABLE transactions AS SELECT * FROM 'data/transactions.parquet'"
-    )
-    transactions_df = conn.execute("SELECT * FROM transactions").df()
-    transactions_df["amount"] = transactions_df["amount"] * 1.1
-    transactions_df.to_parquet("data/transactions_transformed.parquet", index=False)
-
-    # in memory is faster supposedly, but does not persist for forensics or visualization
-    memory_conn = duckdb.connect(":memory:")
-    memory_conn.execute(
-        "CREATE TABLE transactions AS SELECT * FROM 'data/transactions.parquet'"
-    )
-    mem_transactions_df = memory_conn.execute("SELECT * FROM transactions").df()
-    mem_transactions_df["amount"] = mem_transactions_df["amount"] * 1.1
-    mem_transactions_df.to_parquet(
-        "data/transactions_transformed_memory.parquet", index=False
-    )
+    pass
 
 
 if __name__ == "__main__":
